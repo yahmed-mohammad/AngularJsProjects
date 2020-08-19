9607e3b2a6adbda708b5d704e77a3a0aca3fadc5
MFAHMED <MFAHMED@MFAHMED-IN.in.oracle.com>
2020-08-19 16:45:47 +0530

    Narrow Menu App Initial commit, Getting menu details using  service from a remove server

---
+++ 8_NarrowItDownApp/app.js
@@ -0,0 +1,49 @@
+(function() {
+    'use strict';
+    let module = angular.module('NarrowItDownApp',[]);
+
+    module.controller('NarrowItDownController', NarrowItDownController);
+    module.service('MenuSearchService', MenuSearchService);
+
+    //Inject service into Controller
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    //Defining Controller
+    function NarrowItDownController(MenuSearchService) {
+        let narrowCtrl = this;
+        narrowCtrl.item = '';
+
+        narrowCtrl.searchMenu = function() {
+            narrowCtrl.found = MenuSearchService.getMatchedMenuItems(narrowCtrl.item);
+        } 
+    }
+
+    //Inject http service in our service function
+    MenuSearchService.$inject = ['$http'];
+
+    //Defining Service
+    function MenuSearchService($http) {
+        let narrowService = this;
+
+        narrowService.getMatchedMenuItems = function(stringVal) {
+            let responsePromise = $http({
+                method: "GET",
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            });
+
+            let found = [];
+            
+            responsePromise.then(function(response) {
+                let menuItems = response.data.menu_items;
+                for(let i=0; i<menuItems.length; i++) {
+                    let description = menuItems[i].description;
+                    if(description.indexOf(stringVal) !== -1) {
+                        found.push(menuItems[i]);
+                    }
+                }
+            }).catch(function(errorResponse) {
+                console.log('Error');
+            });
+            return found;
+        }
+    }
+})();
